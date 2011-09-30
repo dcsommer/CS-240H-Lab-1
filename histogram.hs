5924ef26b82522b31af8ba98cfad436539b91a84
Daniel Sommermann <dcsommer@gmail.com>
2011-09-29 22:13:41 -0700

    Updated to match spec

--- histogram.hs
+++ histogram.hs
@@ -1,12 +1,20 @@
 import Data.Char
+import Data.List
 import Data.Map (Map, assocs, insertWith, empty, (!))
-data Histogram = Histogram Integer (Map String Integer)
+-- A Histogram data type keeps track of the number of occurances
+-- for the most frequent word, the length of the longest word,
+-- and a Map from words to frequencies
+data Histogram = Histogram Integer Integer (Map String Integer)
 
 histWidth = 60
-wordMaxLen = 12
+wordMaxLen = 30
 tickSpace = 8
 tableWidth = wordMaxLen + 1 + histWidth
 ioUnit = return ()
+
+orderTuple :: (String, Integer) -> (String, Integer) -> Ordering
+orderTuple (_, first) (_, second) = if first < second
+                                    then GT else LT
 
 putChars :: Integral a => Char -> a -> IO ()
 putChars ch times | times > 0 = putChar ch >>
@@ -14,47 +22,53 @@ putChars ch times | times > 0 = putChar ch >>
                   | otherwise = ioUnit
 
 putCharsEnd :: Integral a => Char -> a -> Char -> IO ()
-putCharsEnd ch times end = (putChars ch times) >> putChar end
+putCharsEnd ch times end = putChars ch times >> putChar end
 
 printTick :: RealFrac a => a -> IO a -> String -> IO a
 printTick stepSize x num = do
   remainder <- x
   let real = remainder + stepSize - fromIntegral (length num) in
     let toPrint = floor real in
-      if (toPrint >= 3)
+      if toPrint >= 3
       then do
         putChars ' ' toPrint 
         putStr num
-        return (real - (fromIntegral toPrint))
-      else do
-        return (real + (fromIntegral (length num)))
+        return (real - fromIntegral toPrint)
+      else
+        return (real + fromIntegral (length num))
 
-printLegend maxCount =
-    let stepSize = (fromIntegral histWidth) / (fromIntegral maxCount)
+printLegend maxCount row1Width =
+    let stepSize = fromIntegral histWidth / fromIntegral maxCount
     in do
-      putCharsEnd ' ' wordMaxLen '0'
+      putCharsEnd ' ' (row1Width-1) '0'
       foldl (printTick stepSize) (return 0) (map show [1..maxCount])
       putStr "\n"
 
 printHistogramEntries :: Histogram -> IO ()
-printHistogramEntries (Histogram maxCount entries) = do
-  putCharsEnd '=' tableWidth '\n'
-  foldl (\x (key, count) ->
-        x >> putStr (take (fromIntegral wordMaxLen) key) >>
-        putCharsEnd ' ' (wordMaxLen - fromIntegral(length key)) '|' >>
-        putCharsEnd '*' (div (count * histWidth) maxCount) '\n')
-    ioUnit (assocs entries)
-  printLegend maxCount
-  putCharsEnd '=' tableWidth '\n'
+printHistogramEntries (Histogram maxCount longest entries) = 
+  let row1Width = 1 + min wordMaxLen longest in do
+      putCharsEnd '=' tableWidth '\n'
+      printLegend maxCount row1Width
+      foldl (\x (key, count) ->
+        let word = take (fromIntegral row1Width - 1) key in
+          x >> putStr word >>
+          putChars ' ' (row1Width - fromIntegral (length word)) >>
+          putCharsEnd '#' (div (count * histWidth) maxCount) '\n')
+        ioUnit (sortBy orderTuple (assocs entries))
+      putCharsEnd '=' tableWidth '\n'
 
-createCounts :: Integer -> Map String Integer -> [String] -> Histogram
-createCounts maxCount entries []     = Histogram maxCount entries
-createCounts maxCount entries (x:xs) =
+createCounts :: Integer -> Integer -> 
+                Map String Integer-> [String] -> Histogram
+createCounts maxCount longest entries []
+    = Histogram maxCount longest entries
+createCounts maxCount longest entries (x:xs) =
     let added = insertWith (+) x 1 entries
-    in maxCount `seq` createCounts (max maxCount (added ! x)) added xs
+    in maxCount `seq` longest `seq` createCounts
+           (max maxCount (added ! x))
+           (max longest (fromIntegral (length x))) added xs
 
 generateHistogram :: [String] -> Histogram
-generateHistogram input = createCounts 1 empty input
+generateHistogram = createCounts 1 1 empty
 
 main = getContents >>=
        printHistogramEntries . generateHistogram . words . map toLower
